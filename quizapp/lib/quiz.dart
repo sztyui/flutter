ab7eeff9ee48d74ad26be279797c4f1ec482e3b8
Isti <istiszilagyi@outlook.com>
2026-04-26 20:37:09 +0200

    quiz app with two stateless widgets

---
+++ quizapp/lib/quiz.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:quizapp/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 66, 18, 148),
+                Color.fromARGB(255, 96, 34, 204),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
+    );
+  }
+}
---
+++ quizapp/lib/start_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset('assets/images/quiz-logo.png', width: 300, color: const Color.fromARGB(150, 255, 255, 255),),
+          const SizedBox(height: 80),
+          const Text(
+            'Learn Flutter the fun way!',
+            style: TextStyle(
+              color: Color.fromARGB(255, 207, 177, 255),
+              fontSize: 28,
+            ),
+          ),
+          const SizedBox(height: 30),
+          OutlinedButton.icon(
+            onPressed: () {
+              
+            },
+            style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+            label: const Text('Start Quiz'),
+            icon: const Icon(Icons.arrow_right_alt),
+          ),
+        ],
+      ),
+    );
+  }
+}
