441cd115bda7d244be3674a8bc90787fbf0a1e40
Isti <istiszilagyi@outlook.com>
2026-04-26 19:29:17 +0200

    finished

--- f_app/lib/dice_roller.dart
+++ f_app/lib/dice_roller.dart
@@ -14,12 +14,9 @@ class DiceRoller extends StatefulWidget {
 }
 
 class _DiceRollerState extends State<DiceRoller> {
-
   var currentDiceRoll = 2;
 
-
   void choosePicture() {
-    
     setState(() {
       currentDiceRoll = random.nextInt(6) + 1;
     });
@@ -27,24 +24,30 @@ class _DiceRollerState extends State<DiceRoller> {
 
   @override
   Widget build(context) {
-    return Column(mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Center(child: StyledText("Az első szerelem app a mösszömnek ❤️")),
-                    const SizedBox(height: 20),
-                    Center(child: Image.asset('assets/images/dice-$currentDiceRoll.jpeg', width: 250)),
-                    const SizedBox(height: 100),
-                    Center(
-                      child: TextButton(
-                        onPressed: choosePicture,
-                        style: TextButton.styleFrom(
-                          padding: const EdgeInsets.only(top: 20),
-                          foregroundColor: Colors.white,
-                          textStyle: const TextStyle(fontSize: 28),
-                        ),
-                        child: Text('Nyomd meg a gombot'),
-                      ),
-                    ),
-                  ],
-                );
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Center(child: StyledText("Az első szerelem app a mösszömnek ❤️")),
+        const SizedBox(height: 20),
+        Center(
+          child: Image.asset(
+            'assets/images/dice-$currentDiceRoll.jpeg',
+            width: 250,
+          ),
+        ),
+        const SizedBox(height: 100),
+        Center(
+          child: TextButton(
+            onPressed: choosePicture,
+            style: TextButton.styleFrom(
+              padding: const EdgeInsets.only(top: 20),
+              foregroundColor: Colors.white,
+              textStyle: const TextStyle(fontSize: 28),
+            ),
+            child: Text('Nyomd meg a gombot'),
+          ),
+        ),
+      ],
+    );
   }
 }
